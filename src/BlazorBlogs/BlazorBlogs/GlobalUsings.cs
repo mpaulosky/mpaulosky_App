74af9b46db77b3bf70797d820f4c5ebe44a5e498
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-10-28 10:02:20 -0700

    Added Projects and External Logins

---
+++ src/BlazorBlogs/BlazorBlogs/GlobalUsings.cs
@@ -0,0 +1,13 @@
+// ============================================
+// Copyright (c) 2023. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : mpaulosky_App
+// Project Name :  BlazorBlogs
+// =============================================
+
+global using BlazorBlogs.Client.Pages;
+global using BlazorBlogs.Components;
+global using BlazorBlogs.Data;
+global using BlazorBlogs.Identity;
